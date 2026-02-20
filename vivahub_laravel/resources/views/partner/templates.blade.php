bd2c3a2fbcd7dbaa5888dc8c20e3512b20ce5c18
Raj Hatnagar <raj9860509908@gmail.com>
2026-02-21 02:32:35 +0530

    Force update all files

--- vivahub_laravel/resources/views/partner/templates.blade.php
+++ vivahub_laravel/resources/views/partner/templates.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
-    <title>Template Library - Partner Portal</title>
+    <title>Template Library - Partner Login</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <link href="https://fonts.googleapis.com" rel="preconnect"/>
